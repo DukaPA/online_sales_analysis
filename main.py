4ff3cdc11eb6b75660b90b207eb92e190dd2e0e9
Dusan Veljkovic <dusan.veljkovic.81@gmail.com>
2025-02-24 20:21:12 +0100

    Changed quantity commented display all products and total value

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from product_manager import ProductManager
 def main():
     manager = ProductManager()
     
-    manager.add_product(Product("Coca Cola Zero", 74.99, 1))
-    manager.add_product(Product("PERSIL deterdžent", 799.92, 2))
-    manager.add_product(Product("Čokolada MILKA",  283.99, 5))
-    manager.add_product(Product("Salata ARGETA", 239.99, 5))
+    manager.add_product(Product("Coca Cola Zero", 74.99, 10))
+    manager.add_product(Product("PERSIL deterdžent", 799.92, 12))
+    manager.add_product(Product("Čokolada MILKA",  283.99, 15))
+    manager.add_product(Product("Salata ARGETA", 239.99, 15))
     manager.add_product(Product("Bombonjere MERCI", 559.2, 10))
     
     #Displaying all products
-    print("Display all products")
-    manager.display_all_products()
+    # print("Display all products")
+    # manager.display_all_products()
     
     print(80*"*")
     
     #total value
-    total_value = manager.total_products_value()
-    print(f"Total value of all products: {total_value:.2f} $")
+    # total_value = manager.total_products_value()
+    # print(f"Total value of all products: {total_value:.2f} $")
     
     # Removing a product
     manager.remove_product("Čokolada MILKA")
